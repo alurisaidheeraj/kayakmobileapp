c9df52c223217dc319181d36bfd50f72ebe6c2f2
sai dheeraj aluri <saluri0328@gmail.com>
2022-07-16 12:13:28 -0400

    searchfilter

--- src/components/AirlineList.js
+++ src/components/AirlineList.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios'
 import jsonpAdapter from "axios-jsonp"
+import logo from "./logo.svg"
 
 const AirlineList = () => {
     const [airlineData, setAirlineData] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('')
  useEffect(() =>{
     axios({
         url: "https://www.kayak.com/h/mobileapis/directory/airlines/homework",
@@ -11,15 +13,25 @@ const AirlineList = () => {
         callbackParamName: "jsonp" //callback by default
       }).then(res=> {
         setAirlineData(res.data)
-        console.log(res)
     }).catch(err=> {
         console.log(err)
     })
  })
- console.log(airlineData)
  return (
     <div>
-            {airlineData.map((airline, index) => (
+     <div className="navbar">
+        <img src={logo} alt="" />
+      </div>
+        <input type="text" placeholder="Airline filter" onChange={e => {setSearchTerm(e.target.value)}}/>
+            {airlineData.filter((airline) => {
+                if(searchTerm === "") {
+                    return airline
+                }
+                else if (airline.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                    return airline
+                }
+                return false
+            }).map((airline, index) => (
                 <div key={index}>
                     <img src={"https://kayak.com" + airline.logoURL} alt="" />
                      {airline.name}</div>
