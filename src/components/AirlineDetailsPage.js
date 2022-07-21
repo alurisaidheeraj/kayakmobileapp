c20726179022a4bd0afdec1f03a53efeaf32c7ef
sai dheeraj aluri <saluri0328@gmail.com>
2022-07-20 20:52:46 -0400

    linting changes

--- src/components/AirlineDetailsPage.js
+++ src/components/AirlineDetailsPage.js
@@ -1,56 +1,79 @@
-import React, { useEffect, useState } from 'react';
-import "./AirlineDetailsPage.scss"
+import React, { useEffect, useState } from "react";
+import "./AirlineDetailsPage.scss";
 import { Link } from "react-router-dom";
-import { useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { setAirlineData } from '../store/airlines/action';
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setAirlineData } from "../store/airlines/action";
 
 const AirlineDetailsPage = () => {
-    const { code } = useParams();
-    const [currentAirLine, setCurrentAirLine] = useState({})
-    const airlines = useSelector((state) => state.airlines.airlineData);
-    const dispatch = useDispatch()
-    let textInput = React.createRef();
+  const { code } = useParams();
+  const [currentAirLine, setCurrentAirLine] = useState({});
+  const airlines = useSelector((state) => state.airlines.airlineData);
+  const dispatch = useDispatch();
+  let textInput = React.createRef();
 
-    useEffect(() => {
-        if (airlines && airlines.length === 0) {
-            dispatch(setAirlineData())
-        }
-    }, [airlines, dispatch])
+  useEffect(() => {
+    if (airlines && airlines.length === 0) {
+      dispatch(setAirlineData());
+    }
+  }, [airlines, dispatch]);
 
+  useEffect(() => {
+    if (airlines && airlines.length > 0) {
+      const airline = airlines.filter((r) => r.code === code);
+      if (airline && airline.length > 0) {
+        setCurrentAirLine(airline[0]);
+      }
+    }
+  }, [code, airlines]);
 
-    useEffect(() => {
-        if (airlines && airlines.length > 0) {
-            const airline = airlines.filter(r => r.code === code)
-            if (airline && airline.length > 0) {
-                setCurrentAirLine(airline[0])
-            }
-
-        }
-
-    }, [code, airlines])
-
-
-    return (
-        <div className="airline-details">
-            <i className="arrow left"></i><span className="airline-back-to-list"><Link to="/">Back to list</Link></span>
-            <div className="airline-detail-list">
-             <div className="airline-details-name">
-                <p>{currentAirLine.name} ({currentAirLine.code})</p>
-                <img className='image' src={"https://kayak.com" + currentAirLine.logoURL}  style={{fill:"#FFFFFF"}} alt="" />
-              </div>
-              <div className='airline-details-phone-website'>
-                <p className="airline-phoneNumber">{currentAirLine.phone}</p>
-                <p className="airline-details-site">{currentAirLine.site}</p>
-            </div>
-            <div className="airline-check-flight-status">
-                Check flight status
-            </div>
-                <input type="text" ref={textInput} placeholder="Enter Flight Number..." />
-                <button type="button" onClick={()=> window.open("https://www.kayak.com/tracker" +'/'+currentAirLine.code+'-'+ textInput.current.value +'/'+new Date().toISOString().slice(0, 10))}>Check</button>
+  return (
+    <div className="airline-details">
+      <i className="arrow left"></i>
+      <span className="airline-back-to-list">
+        <Link to="/">Back to list</Link>
+      </span>
+      <div className="airline-detail-list">
+        <div className="airline-details-name">
+          <p>
+            {currentAirLine.name} ({currentAirLine.code})
+          </p>
+          <img
+            className="image"
+            src={"https://kayak.com" + currentAirLine.logoURL}
+            style={{ fill: "#FFFFFF" }}
+            alt=""
+          />
         </div>
+        <div className="airline-details-phone-website">
+          <p className="airline-phoneNumber">{currentAirLine.phone}</p>
+          <p className="airline-details-site">{currentAirLine.site}</p>
+        </div>
+        <div className="airline-check-flight-status">Check flight status</div>
+        <input
+          type="text"
+          ref={textInput}
+          placeholder="Enter Flight Number..."
+        />
+        <button
+          type="button"
+          onClick={() =>
+            window.open(
+              "https://www.kayak.com/tracker" +
+                "/" +
+                currentAirLine.code +
+                "-" +
+                textInput.current.value +
+                "/" +
+                new Date().toISOString().slice(0, 10)
+            )
+          }
+        >
+          Check
+        </button>
       </div>
-    )
-}
+    </div>
+  );
+};
 
-export default AirlineDetailsPage
+export default AirlineDetailsPage;
--- src/components/AirlineList.js
+++ src/components/AirlineList.js
@@ -1,44 +1,62 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg"
-import { Link } from "react-router-dom"
-import "./AirlineList.scss"
-import { useDispatch, useSelector } from 'react-redux';
-import { setAirlineData } from '../store/airlines/action'
+import logo from "./logo.svg";
+import { Link } from "react-router-dom";
+import "./AirlineList.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { setAirlineData } from "../store/airlines/action";
 const AirlineList = () => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const dispatch = useDispatch()
-    const airlines = useSelector((state) => state.airlines.airlineData);
+  const [searchTerm, setSearchTerm] = useState("");
+  const dispatch = useDispatch();
+  const airlines = useSelector((state) => state.airlines.airlineData);
 
-    useEffect(() => {
-        if (airlines && airlines.length === 0) {
-            dispatch(setAirlineData())
-        }
-    }, [airlines, dispatch])
+  useEffect(() => {
+    if (airlines && airlines.length === 0) {
+      dispatch(setAirlineData());
+    }
+  }, [airlines, dispatch]);
 
-    return (
-        <div className="airlinelist">
-            <div className="airlinelistLogo">
-                <img className="kayak-header-logo" src={logo} alt="" />
+  return (
+    <div className="airlinelist">
+      <div className="airlinelistLogo">
+        <img className="kayak-header-logo" src={logo} alt="" />
+      </div>
+      <input
+        className="airline-search-filter"
+        type="text"
+        placeholder="Airline filter..."
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
+      <div className="airlinelistDetails">
+        {airlines
+          .filter((airline) => {
+            if (searchTerm === "") {
+              return airline;
+            } else if (
+              airline.name.toLowerCase().includes(searchTerm.toLowerCase())
+            ) {
+              return airline;
+            }
+            return false;
+          })
+          .map((airline, index) => (
+            <div className="items" key={index}>
+              <div className="aligned">
+                <img
+                  className="airline-image"
+                  src={"https://kayak.com" + airline.logoURL}
+                  alt=""
+                />
+                <Link to={`${airline.code}`}>
+                  {" "}
+                  <span className="airlineName">{airline.name}</span>
+                </Link>
+              </div>
             </div>
-            <input className="airline-search-filter" type="text" placeholder="Airline filter..." onChange={e => { setSearchTerm(e.target.value) }} />
-            <div className="airlinelistDetails">{airlines.filter((airline) => {
-                if (searchTerm === "") {
-                    return airline
-                }
-                else if (airline.name.toLowerCase().includes(searchTerm.toLowerCase())) {
-                    return airline
-                }
-                return false
-            }).map((airline, index) => (
-                <div className="items" key={index}>
-                    <div className="aligned">
-                        <img className="airline-image" src={"https://kayak.com" + airline.logoURL} alt="" />
-                        <Link to={`${airline.code}`}> <span className="airlineName">{airline.name}</span></Link>
-                    </div>
-                </div>
-            ))}
-            </div>
-        </div >
-    )
-}
+          ))}
+      </div>
+    </div>
+  );
+};
 export default AirlineList;
